771be2ff119dab4f35b4a1d3563079a8ebb2a8fa
Pedro Stanzani <pedrohstanzani@gmail.com>
2023-07-12 21:32:26 -0300

    Add GitHub button

--- src/components/PageHeading.jsx
+++ src/components/PageHeading.jsx
@@ -7,9 +7,25 @@ const PageHeading = () => {
         Closest Tailwind color
       </h1>
       <p>
-        Choose a color and find its closest match on the TailwindCSS default
-        color palette.
+        Choose a color and find its closest match on the TailwindCSS{" "}
+        <a
+          href="https://tailwindcss.com/docs/customizing-colors"
+          target="_blank"
+          className="underline hover:font-semibold"
+        >
+          default color palette
+        </a>
+        .
       </p>
+      <div className="my-2">
+        <iframe
+          src="https://ghbtns.com/github-btn.html?user=pedrostanzani&repo=closest-tailwind-color&type=star&count=true"
+          frameborder="0"
+          width="100"
+          height="20"
+          title="GitHub"
+        ></iframe>
+      </div>
     </div>
   );
 };
